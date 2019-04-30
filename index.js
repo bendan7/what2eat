6fcfd1b730dc309b70579e5b6eb406367d1a10ba
unknown <Ben>
2019-04-30 17:54:58 +0300

    eslint config

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+/* eslint-disable import/no-unresolved */
 /**
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './src/components/App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+// eslint-disable-next-line import/no-unresolved
+// eslint-disable-next-line import/no-extraneous-dependencies
+import App from 'src/components/App';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,8 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-else-return */
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/jsx-first-prop-new-line */
+/* eslint-disable max-len */
 /* eslint-disable linebreak-style */
 /* eslint-disable global-require */
 /**
@@ -10,31 +15,31 @@
 
 
 import React from 'react';
-import { Text, View, Dimensions, Image, Animated, PanResponder, ImageBackground } from 'react-native'
+import { Text, View, Dimensions, Image, Animated, PanResponder, ImageBackground } from 'react-native';
 
 
-const SCREEN_HEIGHT = Dimensions.get('window').height
-const SCREEN_WIDTH = Dimensions.get('window').width
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+const SCREEN_WIDTH = Dimensions.get('window').width;
 const Users = [
   { id: '1', name: 'tometo', uri: require('../tometo.jpg') },
   { id: '2', name: 'pasta', uri: require('../pasta.jpg') },
   { id: '3', name: 'Olive-Oil', uri: require('../Olive-Oil.jpg') },
-]
+];
 
 export default class App extends React.Component {
   constructor() {
-    super()
+    super();
 
-    this.position = new Animated.ValueXY()
+    this.position = new Animated.ValueXY();
     this.state = {
       currentIndex: 0,
-    }
+    };
 
     this.rotate = this.position.x.interpolate({
       inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
       outputRange: ['-10deg', '0deg', '10deg'],
       extrapolate: 'clamp',
-    })
+    });
 
     this.rotateAndTranslate = {
       transform: [{
@@ -42,38 +47,39 @@ export default class App extends React.Component {
       },
       ...this.position.getTranslateTransform(),
       ],
-    }
+    };
 
     this.likeOpacity = this.position.x.interpolate({
       inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
       outputRange: [0, 0, 1],
       extrapolate: 'clamp',
-    })
+    });
     this.dislikeOpacity = this.position.x.interpolate({
       inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
       outputRange: [1, 0, 0],
       extrapolate: 'clamp',
-    })
+    });
 
     this.nextCardOpacity = this.position.x.interpolate({
       inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
       outputRange: [1, 0, 1],
       extrapolate: 'clamp',
-    })
+    });
     this.nextCardScale = this.position.x.interpolate({
       inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
       outputRange: [1, 0.8, 1],
       extrapolate: 'clamp',
-    })
+    });
   }
 
   componentWillMount() {
     this.getNextAttToAsk();
     this.PanResponder = PanResponder.create({
 
+      // eslint-disable-next-line no-unused-vars
       onStartShouldSetPanResponder: (evt, gestureState) => true,
       onPanResponderMove: (evt, gestureState) => {
-        this.position.setValue({ x: gestureState.dx, y: gestureState.dy })
+        this.position.setValue({ x: gestureState.dx, y: gestureState.dy });
       },
       onPanResponderRelease: (evt, gestureState) => {
         if (gestureState.dx > 120) { // LIKE CASE
@@ -81,25 +87,25 @@ export default class App extends React.Component {
             toValue: { x: SCREEN_WIDTH + 100, y: gestureState.dy }
           }).start(() => {
             this.setState({ currentIndex: this.state.currentIndex + 1 }, () => {
-              this.position.setValue({ x: 0, y: 0 })
-            })
-          }) // DISLIKE CASE
+              this.position.setValue({ x: 0, y: 0 });
+            });
+          }); // DISLIKE CASE
         } else if (gestureState.dx < -120) {
           Animated.spring(this.position, {
             toValue: { x: -SCREEN_WIDTH - 100, y: gestureState.dy }
           }).start(() => {
             this.setState({ currentIndex: this.state.currentIndex + 1 }, () => {
-              this.position.setValue({ x: 0, y: 0 })
-            })
-          })
+              this.position.setValue({ x: 0, y: 0 });
+            });
+          });
         } else {
           Animated.spring(this.position, {
             toValue: { x: 0, y: 0 },
             friction: 4,
-          }).start()
+          }).start();
         }
       },
-    })
+    });
   }
 
      getNextAttToAsk = () => {
@@ -111,7 +117,7 @@ export default class App extends React.Component {
          console.log(responseJson);
          this.setState({
             data: responseJson
-         })
+         });
       })
       .catch((error) => {
          console.error(error);
@@ -119,25 +125,27 @@ export default class App extends React.Component {
    }
 
 
-
   renderUsers = () => {
     return Users.map((item, i) => {
       if (i < this.state.currentIndex) {
-        return null
-      } else if (i == this.state.currentIndex) {
+        return null;
+      } else if (i === this.state.currentIndex) {
         return (
           <Animated.View
             {...this.PanResponder.panHandlers}
             key={item.id}
             style={[this.rotateAndTranslate, {
-              height: SCREEN_HEIGHT - 120, width: SCREEN_WIDTH, padding: 10, position: 'absolute' }]}>
+              height: SCREEN_HEIGHT - 120, width: SCREEN_WIDTH, padding: 10, position: 'absolute' }]}
+          >
             <Animated.View style={{
-              opacity: this.likeOpacity, transform: [{ rotate: '-30deg' }], position: 'absolute', top: 50, left: 40, zIndex: 1000 }}>
+              opacity: this.likeOpacity, transform: [{ rotate: '-30deg' }], position: 'absolute', top: 50, left: 40, zIndex: 1000 }}
+            >
               <Text style={{ borderWidth: 1, borderColor: 'green', color: 'green', fontSize: 32, fontWeight: '800', padding: 10 }}>LIKE</Text>
             </Animated.View>
 
             <Animated.View style={{
-              opacity: this.dislikeOpacity, transform: [{ rotate: '30deg' }], position: 'absolute', top: 50, right: 40, zIndex: 1000 }}>
+              opacity: this.dislikeOpacity, transform: [{ rotate: '30deg' }], position: 'absolute', top: 50, right: 40, zIndex: 1000 }}
+            >
               <Text style={{ borderWidth: 1, borderColor: 'red', color: 'red', fontSize: 32, fontWeight: '800', padding: 10 }}>NOPE</Text>
             </Animated.View>
 
@@ -145,13 +153,14 @@ export default class App extends React.Component {
               style={{
                 flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 20 }} 
               imageStyle={{ borderRadius: 20 }}
-              source={item.uri} >
-                <View style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'}}>
-                  <Text style={{ fontSize: 40, color:'white'}}>{item.name}</Text>
+              source={item.uri}
+            >
+                <View style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center' }}>
+                  <Text style={{ fontSize: 40, color: 'white' }}>{item.name}</Text>
                 </View>
               </ImageBackground>
           </Animated.View>
-        )
+        );
       } else {
         return (
           <Animated.View
@@ -163,7 +172,8 @@ export default class App extends React.Component {
               width: SCREEN_WIDTH,
               padding: 10,
               position: 'absolute'
-            }]}>
+            }]}
+          >
             <Animated.View style={{ opacity: 0, transform: [{ rotate: '-30deg' }], position: 'absolute', top: 50, left: 40, zIndex: 1000 }}>
               <Text style={{ borderWidth: 1, borderColor: 'green', color: 'green', fontSize: 32, fontWeight: '800', padding: 10 }}>LIKE</Text>
 
@@ -176,12 +186,13 @@ export default class App extends React.Component {
 
             <Image
               style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 20 }}
-              source={item.uri} />
+              source={item.uri}
+            />
 
           </Animated.View>
-        )
+        );
       }
-    }).reverse()
+    }).reverse();
   }
 
   render() {
@@ -197,6 +208,6 @@ export default class App extends React.Component {
           <Text>FOTTER</Text>
         </View>
       </View>
-    )
+    );
   }
 }
